886faf6d8f7c29682dcf14cf4ddd09d334d2438c
Alex Fontan <afontanmoreira@danielcastelao.org>
2020-11-09 11:30:59 +0100

    Boletin7

--- Boletin7/src/com/company/Boletin7_1.java
+++ Boletin7/src/com/company/Boletin7_1.java
@@ -3,7 +3,7 @@ package com.company;
 public class Boletin7_1 {
     public void Ej7_1(int num){
         if (num >= 0){
-            System.out.println("Es positivo.");
+            System.out.println("Es positivo. ");
         }
     }
 }
