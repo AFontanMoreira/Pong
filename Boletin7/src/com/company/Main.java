bb352242db27461fa62fd08ca5eb10e974bc9fbd
Alex Fontan <afontanmoreira@danielcastelao.org>
2020-11-11 12:29:29 +0100

    Boletin7

--- Boletin7/src/com/company/Main.java
+++ Boletin7/src/com/company/Main.java
@@ -8,19 +8,23 @@ public class Main {
     public static void main(String[] args) {
     Scanner dato = new Scanner(System.in);
     //Ejercicio1-------------------------
+    System.out.println("Ejercicio1");
 	Boletin7_1 obj1 = new Boletin7_1();
 	obj1.Ej7_1(Integer.parseInt(JOptionPane.showInputDialog("Dame un numero.")));
     //Ejercicio2-------------------------
-	Boletin7_2 obj2 = new Boletin7_2();
+    System.out.println("Ejercicio2");
+    Boletin7_2 obj2 = new Boletin7_2();
     System.out.println("Primer numero:");
     short num1 = dato.nextShort();
     System.out.println("Segundo numero:");
     short num2 = dato.nextShort();
     obj2.Ej7_2(num1,num2);
     //Ejercicio3------------------------
+    System.out.println("Ejercicio3");
     Boletin7_3 obj3 = new Boletin7_3();
     obj3.Ej7_3(Integer.parseInt(JOptionPane.showInputDialog("Dame un numero.")));
     //Ejercicio4------------------------
+    System.out.println("Ejercicio4");
     Boletin7_4 obj4 = new Boletin7_4();
     String nome1,nome2;
     float peso1,peso2;
@@ -32,6 +36,7 @@ public class Main {
     peso2 = dato.nextFloat();
     obj4.Ej7_4(nome1,peso1,nome2,peso2);
     //Ejercicio5-----------------------
+    System.out.println("Ejercicio5");
     Boletin7_5 obj5 = new Boletin7_5();
     int numero1,numero2,numero3;
         System.out.println("Primer numero");
