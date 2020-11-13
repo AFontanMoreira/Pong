f436f7e40a4906b8dd24c6832a3ead2c544881c7
Alex Fontan <afontanmoreira@danielcastelao.org>
2020-11-13 13:42:25 +0100

    Extra2

--- Extra2/src/com/company/Main.java
+++ Extra2/src/com/company/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner datos = new Scanner(System.in);
+/*
         //Ejercicio1----------------------
 
-        Scanner datos = new Scanner(System.in);
          Extra2_1 obj1 = new Extra2_1();
         boolean padreSocio;
         String preguntaPadreSocio = "";
@@ -24,17 +24,28 @@ public class Main {
               padreSocio=true;
          }else padreSocio=false;
         obj1.Ej1(edad,padreSocio);
-
+*/
         //Ejercicio2---------------------
-
         Extra2_2 obj2 = new Extra2_2();
         int dia,mes,año;
-        System.out.println("Dame el dia de la fecha:");
+        //Parte1
+        /*System.out.println("Dame el dia de la fecha:");
         dia = datos.nextInt();
         System.out.println("Dame el mes de la fecha:");
         mes = datos.nextInt();
         System.out.println("Dame el año de la fecha:");
         año = datos.nextInt();
+         */
+        //Parte2
+        String fecha;
+        System.out.println("Dame la fecha: Ej(00/00/0000)");
+        fecha = datos.next();
+        String[] partes = fecha.split("/");
+        dia = Integer.parseInt(partes[0]);
+        mes = Integer.parseInt(partes[1]);
+        año = Integer.parseInt(partes[2]);
+
+
         boolean valido = obj2.Ej2(dia,mes,año);
         if (valido == true){
             System.out.println("La fecha es valida");
