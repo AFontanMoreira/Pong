9db792b3f9846f199a890bf174ef84b11179f349
Alex Fontan <afontanmoreira@danielcastelao.org>
2020-12-09 14:07:53 +0100

    Boletin11

---
+++ Boletin11/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int numero,intentos,nOpcion;
+    numero=Integer.parseInt(JOptionPane.showInputDialog("Dame un número del 1-50"));
+    intentos=Integer.parseInt(JOptionPane.showInputDialog("Dame el número de intentos"));
+    }
+}
