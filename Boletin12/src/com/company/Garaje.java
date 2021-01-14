8953bbaa2a59ef7d2faa5e07efeadbb44312fe54
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-01-14 13:11:11 +0100

    Boletin12

---
+++ Boletin12/src/com/company/Garaje.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Garaje {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        int plazas=5;
+        Coche bmw=new Coche("8657 AAA");
+        bmw.aparcar(plazas);
+        bmw.salir(plazas);
+    }
+}
